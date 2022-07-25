1e6abff759aeb15a6ee20ea9c768607fddf02989
Amir Ghorbani <AmirGhorbani1002@gmail.com>
2022-07-25 14:02:46 +0430

    add entities

---
+++ src/main/java/entity/Article.java
@@ -0,0 +1,61 @@
+package entity;
+
+import java.sql.Date;
+
+public class Article {
+
+    private int id;
+    private String title;
+    private String content;
+    private Date createDate;
+    private boolean isPublished;
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public boolean isPublished() {
+        return isPublished;
+    }
+
+    public void setPublished(boolean published) {
+        isPublished = published;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
